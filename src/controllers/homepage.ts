4cc85c5bf82ff410ac8edd6768605a87b7940c50
jzy <3011543110@qq.com>
2021-12-13 18:57:07 +0800

    fix: homepage冲突

--- src/controllers/homepage.ts
+++ src/controllers/homepage.ts
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { XRequest } from "../utils/axios"
-import { LoginParams, RegistParams } from "./models/user"
-import { API } from "../enum/api"
-import { message } from "ant-design-vue"
-// 页面跳转
-import { useGo } from "../utils/usePage"
-import { PageEnum } from "../enum/pageEnum"
-
-export async function GetNavItem() {
-  return XRequest({
-    url: API.NAV,
-    method: 'get',
-  }).then(async (data) => {
-    return Promise.resolve(data.data)
-  }).catch(e => {
-    console.log('报错了')
-    console.log(e)
-  })
-}
-
-export async function BookItem(pageNum: number) {
-  return XRequest({
-    url: `${API.BOOKITEM}?pageNum=${pageNum}`,
-    method: 'get',
-  }).then(async (data) => {
-    return Promise.resolve(data.data)
-  }).catch(e => {
-    console.log('报错了')
-    console.log(e)
-  })
-=======
 import { XRequest } from "../utils/axios"
 import { LoginParams, RegistParams } from "./models/user"
 import { API } from "../enum/api"
@@ -84,5 +52,4 @@ export async function SearchBook(BookName: String) {
     console.log('报错了')
     console.log(e)
   })
->>>>>>> tmp
 }
